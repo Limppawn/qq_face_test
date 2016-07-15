07e4c641daf06c3c1dc46bb997de3dae5945a0da
梁志成 <Lzc_file@liangzhichengdemacbook-pro.local>
2016-07-15 20:33:24 +0800

    Initial Commit

---
+++ qq face/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qq face
+//
+//  Created by 梁志成 on 16/7/15.
+//  Copyright © 2016年 梁志成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ qq face/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  qq face
+//
+//  Created by 梁志成 on 16/7/15.
+//  Copyright © 2016年 梁志成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
